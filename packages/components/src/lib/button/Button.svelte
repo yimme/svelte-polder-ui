0ccbb1bfa5cad137c08e579eac2db379a5f1658a
Jimme <hendrixjimme@gmail.com>
2022-10-27 15:17:14 +0200

    add storybook

---
+++ packages/components/src/lib/button/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts" context="module">
+	export type ButtonVariant = 'default' | 'ghost';
+</script>
+
+<script lang="ts">
+	import type { Color } from '$lib/types/color';
+
+	import styles from './button.module.scss';
+
+	export let variant: ButtonVariant;
+	export let color: Color;
+</script>
+
+<button class="button" class:variant class:color>
+	<slot />
+</button>
---
+++ packages/components/src/lib/button/button.stories.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import Button from './Button.svelte';
+</script>
+
+<Meta title="Button" component={Button} />
+
+<Template let:args>
+	<Button {...args}>I am button</Button>
+</Template>
+
+<Story name="Default" />
