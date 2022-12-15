f4d3953bd0e8afe0a3daaaf3814492eda1a773c1
Jimme <hendrixjimme@gmail.com>
2022-12-15 19:08:36 +0100

    add alert component and story

---
+++ packages/components/src/lib/components/alert/Alert.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import type { Color } from '../../types/color';
+
+	export let color: Color = 'primary';
+	export let stretch: Boolean = false;
+
+	let classList = ['alert', color, stretch ? 'stretch' : null].filter(Boolean).join(' ');
+</script>
+
+<div class={classList}>
+	<span class="alert__message">
+		<slot />
+	</span>
+</div>
+
+<style lang="scss" scoped>
+	.alert {
+		display: inline-flex;
+		box-sizing: border-box;
+		padding: var(--space-3) var(--space-4);
+		background: var(--color-primary-100);
+		color: var(--color-primary-500);
+		border-radius: var(--radius-1);
+		gap: var(--space-3);
+
+		&.stretch {
+			display: flex;
+		}
+
+		&__message {
+			line-height: var(--line-height-body);
+		}
+	}
+</style>
---
+++ packages/components/src/lib/components/alert/alert.stories.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import Alert from './Alert.svelte';
+
+	const meta = {
+		title: 'Alert',
+		component: Alert,
+		argTypes: {}
+	};
+</script>
+
+<Meta {...meta} />
+
+<Template let:args>
+	<Alert {...args}>The server was unable to handle your request</Alert>
+</Template>
+
+<Story name="Default" args={{}} />
