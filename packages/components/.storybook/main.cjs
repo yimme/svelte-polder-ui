abdb253ad162e4f91ade73d2c58a6bb3ca3f5554
Jimme <hendrixjimme@gmail.com>
2023-01-11 21:15:55 +0100

    add storybook dark mode addon

--- packages/components/.storybook/main.cjs
+++ packages/components/.storybook/main.cjs
@@ -5,7 +5,8 @@ module.exports = {
 	addons: [
 		'@storybook/addon-links',
 		'@storybook/addon-essentials',
-		'@storybook/addon-interactions'
+		'@storybook/addon-interactions',
+		'@storybook-dark-mode'
 	],
 	framework: '@storybook/svelte',
 	core: {
